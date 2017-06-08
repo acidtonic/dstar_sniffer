51d05e22f8e71771eca90f0ed7069c4cf226c52d
eliel <esardanons@despegar.com>
2017-06-08 20:06:48 -0300

    more packet manipulation.

--- dstar.py
+++ dstar.py
@@ -11,8 +11,16 @@ def parse(data):
 		(packet[9] == 0x30 or packet[9] == 0x13 or packet[9] == 0x16):
 			# DV packet
 			print "DV PACKET!!!"
-			print "Data (hex): " + ":".join("{:02x}".format(ord(c)) for c in data)
-			print "START from rptr: cntr=%02x %02x, streamID=%d,%d, flags=%02x:%02x:%02x, my=%.8s, sfx=%.4s, ur=%.8s, rpt1=%.8s, rpt2=%.8s" % (pkt[4], pkt[5], pkt[14], pkt[15], pkt[17], pkt[18], pkt[19], pkt[44], pkt[52], pkt[36], pkt[28], pkt[20])
+
+			if data_len == 58:
+				my = ''.join(packet[44:8])
+				sfx = ''.join(packet[52:4]
+				ur = ''.join(packet[36:8])
+				rpt1 = ''.join(packet[28:8])
+				rpt2 = ''.join(packet[20:8])
+	
+
+				print "START from rptr: cntr=%02x %02x, streamID=%d,%d, flags=%02x:%02x:%02x, my=%.8s, sfx=%.4s, ur=%.8s, rpt1=%.8s, rpt2=%.8s" % (packet[4], packet[5], packet[14], packet[15], packet[17], packet[18], packet[19], my, sfx, ur, rpt1, rpt2)
 		
 
 
