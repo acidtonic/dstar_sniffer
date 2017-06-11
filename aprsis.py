4d50cee1b275d60dc14f3060704e7c29dae6c512
eliel <esardanons@despegar.com>
2017-06-10 21:55:59 -0300

    Typo....

--- aprsis.py
+++ aprsis.py
@@ -8,7 +8,7 @@ class AprsIS:
 		self.aprs_connection.start()
 
 	def send_beacon(self, callsign, sfx, message, gpgga):
-		position = nmea.gpgga_get_position(gpgga_sentence)
+		position = nmea.gpgga_get_position(gpgga)
 		print "Send beacon"
 		aprs_frame = callsign+'>APK'+sfx+':!'+gpgga['lat'] + gpgga['lat_coord'] + '/'+gpgga['long']+gpgga['long_coord']+'Da/A=' + gpgga['height'] + '>' + message
 		self.aprs_connection.send(aprs.Frame(aprs_frame))
