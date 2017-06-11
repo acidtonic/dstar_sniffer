5489e90bc6073306e0352dfae02e458bce7a1fd3
eliel <esardanons@despegar.com>
2017-06-11 19:23:28 -0300

    Add logging to aprsis.

--- aprsis.py
+++ aprsis.py
@@ -3,14 +3,18 @@ import nmea
 
 class AprsIS:
 
-	def __init__(self, callsign, password):
+	logger = None
+
+	def __init__(self, logger, callsign, password):
+		self.logger = logger
 		self.aprs_connection = aprs.TCP(callsign, password)
 		self.aprs_connection.start()
 
 	def send_beacon(self, callsign, sfx, message, gpgga):
 		position = nmea.gpgga_get_position(gpgga)
 		aprs_frame = callsign+'>APK'+sfx+':!'+position['lat'] + position['lat_coord'] + '\\'+position['long']+position['long_coord']+'a/A=' + position['height'] + message
+		self.logger.info("Sending APRS Frame: " + aprs_frame)
 		try:
 			self.aprs_connection.send(aprs.Frame(aprs_frame))
 		except:
-			print "Invalid aprs frame: " + aprs_frame
+			self.logger.info("Invalid aprs frame: " + aprs_frame)
