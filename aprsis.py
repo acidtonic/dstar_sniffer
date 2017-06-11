52b68ab18eecf797cdeacbadb97705352a0d652a
eliel <esardanons@despegar.com>
2017-06-10 21:54:56 -0300

    Fix compile error.

--- aprsis.py
+++ aprsis.py
@@ -7,7 +7,7 @@ class AprsIS:
 		self.aprs_connection = aprs.TCP(callsign, password)
 		self.aprs_connection.start()
 
-	def send_beacon(self, callsign, sfx, message, gpgga_sentence):
+	def send_beacon(self, callsign, sfx, message, gpgga):
 		position = nmea.gpgga_get_position(gpgga_sentence)
 		print "Send beacon"
 		aprs_frame = callsign+'>APK'+sfx+':!'+gpgga['lat'] + gpgga['lat_coord'] + '/'+gpgga['long']+gpgga['long_coord']+'Da/A=' + gpgga['height'] + '>' + message
