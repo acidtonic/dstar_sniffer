6edff6d17e7362090e9ea422c73bd8306f0dd39e
eliel <esardanons@despegar.com>
2017-06-12 13:36:52 -0300

    Add DSTAR* to aprs path.

--- aprsis.py
+++ aprsis.py
@@ -12,7 +12,7 @@ class AprsIS:
 
 	def send_beacon(self, callsign, sfx, message, gpgga):
 		position = nmea.gpgga_get_position(gpgga)
-		aprs_frame = callsign+'>APK'+sfx+':!'+position['lat'] + position['lat_coord'] + '\\'+position['long']+position['long_coord']+'a/A=' + position['height'] + message
+		aprs_frame = callsign+'>APK'+sfx+',DSTAR*:!'+position['lat'] + position['lat_coord'] + '\\'+position['long']+position['long_coord']+'a/A=' + position['height'] + message
 		self.logger.info("Sending APRS Frame: " + aprs_frame)
 		try:
 			self.aprs_connection.send(aprs.Frame(aprs_frame))
