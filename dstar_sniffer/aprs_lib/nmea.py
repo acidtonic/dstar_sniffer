5ee86a977235a042da09d28ba61a35d814f990bd
eliel <esardanons@despegar.com>
2017-06-15 11:37:38 -0300

    Fix some code issues.

--- dstar_sniffer/aprs_lib/nmea.py
+++ dstar_sniffer/aprs_lib/nmea.py
@@ -7,9 +7,10 @@ def gpgga_get_position(gpgga_sentence):
 	position['lat_coord'] = sentence[3]
 	position['long'] = sentence[4]
 	position['long_coord'] = sentence[5]
-	position['height'] = str(int(float(sentence[11]) * 3.28084)).zfill(6)
+	if len(sentence[11]) > 0:
+		position['height'] = str(int(float(sentence[11]) * 3.28084)).zfill(6)
 	return position
 
 if __name__ == "__main__":
-	print gpgga_get_position("$GPGGA,142353.00,3436.93,S,05822.72,W,1,06,2.4,55.5,M,13.2,M,,*54")
+	print gpgga_get_position("$GPGGA,,3434.28,S,05829.35,W,,,,176,M,,,,,*39")
 
