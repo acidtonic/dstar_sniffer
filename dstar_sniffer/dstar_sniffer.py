2b384dde64154bf3e35b1ea8054742fb9039a2bc
eliel <esardanons@despegar.com>
2017-06-16 16:53:03 -0300

    Support arguments to start the dstar_sniffer daemon.

--- dstar_sniffer/dstar_sniffer.py
+++ dstar_sniffer/dstar_sniffer.py
@@ -71,4 +71,17 @@ class DStarSniffer(Daemon):
 
 def main():
 	daemon = DStarSniffer('/var/run/dstar_sniffer.pid')
-	daemon.start()
+	if len(sys.argv) == 2:
+		if 'start' == sys.argv[1]:
+			daemon.start()
+		elif 'stop' == sys.argv[1]:
+			daemon.stop()
+		elif 'restart' == sys.argv[1]:
+			daemon.restart()
+		else:
+			print "Unknown command"
+			sys.exit(2)
+		sys.exit(0)
+	else:
+		print "usage: %s start|stop|restart" % sys.argv[0]
+		sys.exit(2)
