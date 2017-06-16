aadf97dd705f3bcee93b21c6910bed023d0c167f
eliel <esardanons@despegar.com>
2017-06-16 17:24:09 -0300

    Update version to 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='DStarSniffer',
-      version='1.0',
+      version='1.1',
       description='DStar repeater controller sniffer',
       url='http://github.com/elielsardanons/dstar-sniffer',
       author='Eliel Sardanons LU1ALY',
